3d421e6d040711a65df55a599c6d4485ddd6e6d4
Amanda <albullington@gmail.com>
2018-05-16 11:14:59 -0700

    server side rendering fully functioning

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const React = require('react');
 
-// const Layout = require('../templates/layout');
 const App = require('../../public/dist/bundle-server');
+const { Html } = require('../templates/layout');
 const { renderToString } = require('react-dom/server');
-
-// const { renderComponent } = require('../helpers/serverSideRender');
+const { ServerStyleSheet } = require('styled-components');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  const sheet = new ServerStyleSheet();
   const appClass = App.default;
-  const component = renderToString(React.createElement(appClass));
-  res.status(200).send(component);
-  // { body, styles, title }
+  const body = renderToString(sheet.collectStyles(React.createElement(appClass)));
+  const styles = sheet.getStyleTags();
+  const title = 'Server Side Rendering';
+  res.status(200).send(Html({
+    body,
+    styles,
+    title,
+  }));
 })
   .post((req, res) => {
     res.status(201).send({ data: 'Posted!' });
--- server/templates/layout.js
+++ server/templates/layout.js
@@ -1,22 +1,24 @@
-module.exports.Layout = ({ body, styles, title }) => `
+module.exports.Html = ({ body, styles, title }) => `
 <!doctype html>
 <html>
 <head>
     <title>${title}</title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css"> <!-- load bootstrap css -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> <!-- load fontawesome -->
-    <div id='root'>${body}</div>
-    <script src="./bundle-server.js"></script>
     <style>
         body        { padding-top:80px; }
+        ${styles}
     </style>
 </head>
 <body>
 <div class="container">
+
+    <div id='root'>${body}</div>
     <style>
-      ${styles}
+      body        { background-color:#87CEEB; }
   </style>
   </div>
+
   </body>
   </html>
 `;
